502ebcd7d1e105ee2d7e0970a5642ab60b98aa7e
Edinunzio <e.dinunzio@gmail.com>
2016-03-24 22:07:18 -0400

    get page contents functionality added, stubbing out tests for getting list of urls

--- crawler.py
+++ crawler.py
@@ -19,26 +19,43 @@ class Crawler(object):
     def __init__(self):
         self.buffer = BytesIO()
         self.crl = pycurl.Curl()
-
-    def get_page(self):
-        self.status_code = 200
-        return self
-        #request = {}
-        #request['status_code'] = 200
-        #return request
+        self.base_url = 'http://www.oldclassiccar.co.uk/forum/phpbb/phpBB2/viewtopic.php?t=12591'
+        self.fieldnames = ['ID', 'Name', 'Date', 'Content']
 
 
-    """
-    buffer = BytesIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'http://pycurl.io/')
-    c.setopt(c.WRITEDATA, buffer)
-    c.perform()
-    c.close()
+    def get_request(self, url):
+        self.crl.setopt(self.crl.URL, url)
+        self.crl.setopt(self.crl.WRITEDATA, self.buffer)
+        self.crl.perform()
+        self.crl.close()
 
-    body = buffer.getvalue()
-    # Body is a byte string.
-    # We have to know the encoding in order to print it to a text file
-    # such as standard output.
-    print(body.decode('iso-8859-1')
-    """
+        body = self.buffer.getvalue()
+        self.body = body.decode('iso-8859-1')
+        return self.body
+
+    def get_pages(self, base_url):
+        return ['a', 'b', 'c', 'd']
+
+"""
+# row should look like:
+# 87120;"Rick";"Mon Sep 24, 2012 4:53 pm";"Tonight, 8pm, might be worth a look...?\n\nRJ"
+# fieldnames: 
+    * post id
+    * name
+    * date of the post (in text form or as is)  
+    * post body
+
+fieldnames = ['ID', 'Name', 'Date', 'Content']
+
+def to_csv(scraped, name):
+    f = open('forum.csv', 'wt')
+    writer = csv.writer(f)
+    writer.writeheader()
+    for _, post in scraped.items():
+        #writer.writerow(['ID', 'Name', 'Date', 'Content'])
+        
+        writer.writerow(post)
+    f.close()
+
+
+"""
--- test_crawler.py
+++ test_crawler.py
@@ -6,6 +6,11 @@ class CrawlerTest(TestCase):
         self.crawler = Crawler()
         self.base_url = 'http://www.oldclassiccar.co.uk/forum/phpbb/phpBB2/viewtopic.php?t=12591'
 
-    def test_get_page_returns_valid_request(self):
-        page = self.crawler.get_page()
-        self.assertEqual(page.status_code, 200)
+    def test_get_page_returns_html(self):
+        page = self.crawler.get_request(self.base_url)
+        self.assertIn('</html>', page)
+
+    def test_get_pages_returns_url_list(self):
+        url_list = self.crawler.get_pages(self.base_url)
+        self.assertIs(type(url_list), list)
+
